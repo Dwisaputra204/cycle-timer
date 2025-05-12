07205c274effd06eaf4bf1fada83227f1348b6f9
Dwi Saputra <dwisaputra204@gmail.com>
2025-05-13 07:19:26 +0800

    fix icon & splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,19 @@ const config: CapacitorConfig = {
   appId: "com.dwisaputra.cycletimer",
   appName: "cycle-timer",
   webDir: "dist",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Durasi tampilan splash dalam milidetik
+      launchAutoHide: true,
+      backgroundColor: "#ffffff", // Warna background
+      androidSplashResourceName: "splash", // Nama resource
+      androidScaleType: "CENTER_CROP", // Tipe scaling
+      showSpinner: true, // Tampilkan spinner
+      androidSpinnerStyle: "large", // Style spinner (Android)
+      iosSpinnerStyle: "large", // Style spinner (iOS)
+      spinnerColor: "#999999", // Warna spinner
+    },
+  },
 };
 
 export default config;
